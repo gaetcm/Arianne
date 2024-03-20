571449890315c079180734b5575d931324e5913c
gaetan <courtemanche.gaetan@gmail.com>
2024-03-20 14:39:05 +0100

    first commit

---
+++ ariane-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
+  return (
+    <>
+      <div className="container">
+        <div className="bouttons">
+          <button
+            onClick={() => {
+              setSwitch1(true);
+            }}
+            style={{ backgroundColor: switch1 === true ? "purple" : "" }}
+          >
+            ON
+          </button>
+
+          <button>ON</button>
+          <button>ON</button>
+          <button>OFF</button>
+          <button>OFF</button>
+          <button>OFF</button>
+        </div>
+        <div className="red">NO WAY !</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
